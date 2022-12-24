93861ba83d61a219e7180da7848a6a24d2ab4836
sungmin choi <sungmin@thetrive.com>
2022-12-24 15:40:50 +0900

    feat) svg 적용 가능하게 끔 설정

--- App.tsx
+++ App.tsx
@@ -1,18 +1,56 @@
 import * as React from 'react';
+import {Text, View} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
-import {SafeAreaView, Text} from 'react-native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import SvgIcon from './components/SvgIcon';
+
+function HomeScreen() {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Home!</Text>
+      <SvgIcon name="calendar" />
+    </View>
+  );
+}
+
+function EditScreen() {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Edit!</Text>
+    </View>
+  );
+}
+
+function DibsScreen() {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Dibs!</Text>
+    </View>
+  );
+}
+
+function MyScreen() {
+  return (
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>My Page!</Text>
+    </View>
+  );
+}
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <SafeAreaView
-        style={{
-          flex: 1,
-          justifyContent: 'center',
-          alignItems: 'center',
+      <Tab.Navigator
+        screenOptions={{
+          headerShown: false,
         }}>
-        <Text>App</Text>
-      </SafeAreaView>
+        <Tab.Screen name="홈" component={HomeScreen} />
+        <Tab.Screen name="작성" component={EditScreen} />
+        <Tab.Screen name="찜" component={DibsScreen} />
+        <Tab.Screen name="마이" component={MyScreen} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
---
+++ res/index.tsx
@@ -0,0 +1,2 @@
+export {default as calendar} from './calendar.svg';
+export {default as Logo} from './Logo.svg';

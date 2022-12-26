e6971df8844801678812c6afc5c849fb67d4d02a
sungmin choi <namja306@naver.com>
2022-12-26 11:45:36 +0900

    머지 변경

--- utils/validation.ts
+++ utils/validation.ts
@@ -1,6 +1,5 @@
 export const emailValidate = (email: string) => {
   const expression = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
   return expression.test(email.toLowerCase());
-  //변경
-  //변경2
+  //머지 변경
 };
